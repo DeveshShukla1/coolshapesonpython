94f078c1e840b551a234dcbfe6c9571e8ce2adbc
Devesh Shukla <136174246+DeveshShukla1@users.noreply.github.com>
2023-06-10 18:47:58 +0530

    Add files via upload

---
+++ newsketch-1.py
@@ -0,0 +1,15 @@
+
+import turtle
+t=turtle.Turtle()
+move=250
+for i in range(360):
+    t.pendown()
+    t.right(move)
+    t.forward(100)
+    t.right(30)
+    t.forward(60)
+    t.left(30)
+    t.forward(30)
+    t.penup()
+    t.home()
+    move= move+2
